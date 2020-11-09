22faac9707245ab85492e215f56cb1ffef0e42fc
Adrien Lapasset <aflapasset@gmail.com>
2020-11-09 17:34:16 +0100

    retour Monopetit

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ $(document).ready(function () {
     $(".search").toggleClass("search--active")
   });
 
+  console.log($(window).width())
   // Clear search
   $('.search__clear').click(function () {
-    $('.search__input').val('').trigger('change').focus();
+    if ($(window).width() >= 992) {
+      $('.search__input').val('').trigger('change').focus();
+    } else {
+      $(".search").toggleClass("search--active")
+    }
   });
 
 });
