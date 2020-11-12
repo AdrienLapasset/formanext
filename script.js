1ea1725facce18daa8a7ec97a71bd4762a564acd
Adrien Lapasset <aflapasset@gmail.com>
2020-11-12 13:01:16 +0100

    retours Monopetit

--- script.js
+++ script.js
@@ -8,9 +8,8 @@ $(document).ready(function () {
   console.log($(window).width())
   // Clear search
   $('.search__clear').click(function () {
-    if ($(window).width() >= 992) {
-      $('.search__input').val('').trigger('change').focus();
-    } else {
+    $('.search__input').val('').trigger('change').focus();
+    if ($(window).width() < 992) {
       $(".search").toggleClass("search--active")
     }
   });
