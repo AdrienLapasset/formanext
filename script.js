625eedc05749012b649498c204c920b61ed573ee
Adrien Lapasset <aflapasset@gmail.com>
2020-12-03 18:21:47 +0100

    add steps to form

--- script.js
+++ script.js
@@ -13,13 +13,8 @@ $(document).ready(function () {
     }
   });
 
-
-
-
-
-
   // Formulaire
-  var step = 1
+  var step = 3
   handleStep(step)
 
   $("#testForm").submit(function (event) {
@@ -30,28 +25,44 @@ $(document).ready(function () {
     }
   });
 
+  $("#prevBtn").click(function () {
+    step--
+    handleStep(step)
+  })
+
   function handleStep(step) {
     if (step === 1) {
       $("#prevBtn").hide();
       $("#step1").show();
       $("#step2").hide();
       $("#step3").hide();
+      $("#step4").hide();
+      $("#step1 input").prop('required', true);
     }
     if (step === 2) {
+      $("#prevBtn").show();
       $("#step1").hide();
       $("#step2").show();
       $("#step3").hide();
+      $("#step4").hide();
+      $("#step2 input").prop('required', true);
     }
     if (step === 3) {
       $("#step1").hide();
       $("#step2").hide();
       $("#step3").show();
+      $("#step4").hide();
+      $("#step3 input").prop('required', true);
+      $("#step3 select").prop('required', true);
+    }
+    if (step === 4) {
+      $("#step1").hide();
+      $("#step2").hide();
+      $("#step3").hide();
+      $("#step4").show();
+      $("#step4 input").prop('required', true);
+      $("#nextBtn").hide();
     }
   }
-
-  $("#prevBtn").click(function () {
-
-  })
-
 
 });
