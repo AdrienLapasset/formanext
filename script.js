cc9c0699ac3262f15c664fd5593c2502982e7fc0
Adrien Lapasset <aflapasset@gmail.com>
2020-12-07 13:07:35 +0100

    add bezier animation to jquery

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ $(document).ready(function () {
   });
 
   var step = 1
+  const bez = [.25, .8, .25, 1]
   initStep()
 
   $("#nextBtn, #nextChevron").click(function () {
@@ -143,14 +144,14 @@ $(document).ready(function () {
     $(`#step${step - 1}`).animate({
       left: '-20px',
       opacity: '0',
-    }, 400, function () {
+    }, 400, $.bez(bez), function () {
       $(this).css({ "left": "-20px" });
       $(this).hide();
       $(`#step${step}`).css("display", "flex");
       $(`#step${step}`).animate({
         left: '0',
         opacity: '1',
-      }, 400);
+      }, 400, $.bez(bez));
     });
   }
 
@@ -158,13 +159,13 @@ $(document).ready(function () {
     $(`#step${step + 1}`).animate({
       left: '20px',
       opacity: '0',
-    }, 400, function () {
+    }, 400, $.bez(bez), function () {
       $(this).hide();
       $(`#step${step}`).css({ "display": "flex" });
       $(`#step${step}`).animate({
         left: '0',
         opacity: '1',
-      }, 400);
+      }, 400, $.bez(bez));
     });
   }
 
